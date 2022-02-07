a6e95db6e65ea9bf8d65db007c919832f04908e1
sunny <mynamesunpower@gmail.com>
2022-02-07 13:08:42 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import './styles.css';
 
-const $app = document.getElementById('app');
+const newsURL = 'https://api.hnpwa.com/v0/news/1.json';
+const $app = document.getElementById('root');
 
-$app.innerHTML = `
-<h1>Hello</h1>
+const ajax = new XMLHttpRequest();
+ajax.open('GET', newsURL, false); // false 동기
+ajax.send();
+
+const newsfeed = JSON.parse(ajax.response);
+console.log(newsfeed);
+
+let appInnerHTML = `<ul>
+  ${newsfeed.map((item) => `<li>${item.title}</li>`).join('')}
+</ul>
 `;
+
+$app.innerHTML = appInnerHTML;
