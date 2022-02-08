187d35d07d4c8384b83f86e14dc8b3233753eabf
sunny <mynamesunpower@gmail.com>
2022-02-08 13:28:51 +0900

    feat: Typing

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,69 @@
-const container = document.getElementById('root');
-const ajax = new XMLHttpRequest();
+type Store = {
+  currentPage: number;
+  feeds: NewsFeed[];
+};
+
+type News = {
+  id: number;
+  title: string;
+  url: string;
+  user: string;
+  time_ago: string;
+  content: string;
+};
+
+type NewsFeed = News & {
+  comments_count: number;
+  points: number;
+  read?: boolean;
+};
+
+type NewsDetail = News & {
+  comments: NewsComment[];
+};
+
+type NewsComment = News & {
+  comments: NewsComment[];
+  level: number;
+};
+
+const container: HTMLElement | null = document.getElementById('root');
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: [],
 };
 
-const getData = (url) => {
+const getData = <AjaxResponse>(url: string): AjaxResponse => {
   ajax.open('GET', url, false);
   ajax.send();
 
   return JSON.parse(ajax.response);
 };
 
-const makeFeeds = (feeds) => {
+const makeFeeds = (feeds: NewsFeed[]): NewsFeed[] => {
   for (let i = 0; i < feeds.length; i += 1) {
     feeds[i].read = false;
   }
   return feeds;
 };
 
-const newsFeed = () => {
-  let newsFeed = store.feeds;
+const updateView = (html: string): void => {
+  if (container) {
+    container.innerHTML = html;
+  } else {
+    console.error('');
+  }
+};
+
+const newsFeed = (): void => {
+  let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
 
   if (newsFeed.length === 0) {
-    newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));
+    newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));
   }
 
   let template = `
@@ -87,12 +124,34 @@ const newsFeed = () => {
   template = template.replace('{{__prev_page__}}', String(prev_page));
   template = template.replace('{{__next_page__}}', String(next_page));
 
-  container.innerHTML = template;
+  updateView(template);
 };
 
-const newsDetail = () => {
+const makeComment = (comments: NewsComment[]): string => {
+  const commentString = [];
+
+  for (let i = 0; i < comments.length; i++) {
+    const comment: NewsComment = comments[i];
+    commentString.push(`
+      <div style="padding-left: ${comment.level * 40}px;" class="mt-4">
+        <div class="text-gray-400">
+          <i class="fa fa-sort-up mr-2"></i>
+          <strong>${comment.user}</strong> ${comment.time_ago}
+        </div>
+        <p class="text-gray-700">${comment.content}</p>
+      </div>      
+    `);
+    if (comment.comments.length > 0) {
+      commentString.push(makeComment(comment.comments));
+    }
+  }
+
+  return commentString.join('');
+};
+
+const newsDetail = (): void => {
   const id = window.location.hash.substr(7);
-  const newsContent = getData(CONTENT_URL.replace('@id', id));
+  const newsContent = getData<NewsDetail>(CONTENT_URL.replace('@id', id));
   let template = `
     <div class="bg-gray-600 min-h-screen pb-8">
       <div class="bg-white text-xl">
@@ -129,34 +188,12 @@ const newsDetail = () => {
     }
   }
 
-  const makeComment = (comments, called = 0) => {
-    const commentString = [];
-
-    for (let i = 0; i < comments.length; i++) {
-      commentString.push(`
-        <div style="padding-left: ${called * 40}px;" class="mt-4">
-          <div class="text-gray-400">
-            <i class="fa fa-sort-up mr-2"></i>
-            <strong>${comments[i].user}</strong> ${comments[i].time_ago}
-          </div>
-          <p class="text-gray-700">${comments[i].content}</p>
-        </div>      
-      `);
-      if (comments[i].comments.length > 0) {
-        commentString.push(makeComment(comments[i].comments, called + 1));
-      }
-    }
-
-    return commentString.join('');
-  };
-
-  container.innerHTML = template.replace(
-    '{{__comments__}}',
-    makeComment(newsContent.comments),
+  updateView(
+    template.replace('{{__comments__}}', makeComment(newsContent.comments)),
   );
 };
 
-const router = () => {
+const router = (): void => {
   const routePath = window.location.hash;
   console.log(routePath);
 
